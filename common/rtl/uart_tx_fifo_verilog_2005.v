387d07182aab5b29cd41eb5f2e159974689eb8bb
verification-explorer <36517349+verification-explorer@users.noreply.github.com>
2025-08-06 11:28:47 +0300

    Update uart_tx_fifo_verilog_2005.v

--- common/rtl/uart_tx_fifo_verilog_2005.v
+++ common/rtl/uart_tx_fifo_verilog_2005.v
@@ -1,3 +1,29 @@
+//----------------------------------------------------------------------
+//   Copyright 2011 Mentor Graphics Corporation
+//   All Rights Reserved Worldwide
+//
+// Modifications:
+// - Converted from SystemVerilog to Verilog-2005 for synthesis compatibility
+//
+//   Licensed under the Apache License, Version 2.0 (the
+//   "License"); you may not use this file except in
+//   compliance with the License.  You may obtain a copy of
+//   the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in
+//   writing, software distributed under the License is
+//   distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+//   CONDITIONS OF ANY KIND, either express or implied.  See
+//   the License for the specific language governing
+//   permissions and limitations under the License.
+//----------------------------------------------------------------------
+
+//
+// This is a 16 deep 8 bit wide FIFO
+// that keeps track of its size
+//
 module uart_tx_fifo(
     input clk,
     input rstn,
@@ -56,3 +82,4 @@ module uart_tx_fifo(
   end
 
 endmodule
+
